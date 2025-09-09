e8662097121861d638522ffee8ccddb98b289d65
syed momin <syedmomin168@gmail.com>
2025-09-09 20:07:15 +0500

    done

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,12 +3,12 @@ import config from "./config";
 
 const dataSource = new DataSource({
   type: "postgres",
-  url: 'postgresql://neondb_owner:npg_Jo3LAvgFs9SB@ep-billowing-meadow-ad8bmlt8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
-  // host: config.dbHost,
-  // port: config.dbPort,
-  // username: config.dbUsername,
-  // password: config.dbPassword,
-  // database: config.dbName,
+  // url: 'postgresql://neondb_owner:npg_Jo3LAvgFs9SB@ep-billowing-meadow-ad8bmlt8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  host: config.dbHost,
+  port: config.dbPort,
+  username: config.dbUsername,
+  password: config.dbPassword,
+  database: config.dbName,
   logging: config.dbLogging as boolean,
   synchronize: config.dbSynchronize as boolean,
   entities: ['src/entities/*.ts'],
